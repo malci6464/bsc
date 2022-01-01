f9cf5aff4755f94a003f8bbd5a8ffd97a6551c91
Malcolm <malc_isa@protonmail.com>
2022-01-01 21:21:29 +0100

    init app

---
+++ bsc/tailwind.config.js
@@ -0,0 +1,14 @@
+// ./tailwind.config.js
+
+
+module.exports = {
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
